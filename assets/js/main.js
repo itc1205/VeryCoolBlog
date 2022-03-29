688ab58a7c2441f5873f2b6884f6c08a60cf3728
ItC <ivan120536@gmail.com>
2022-03-29 22:44:37 +0400

    second day commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,3 +58,23 @@ themeToggleBtn.addEventListener("click", () => {
     localStorage.removeItem("currentTheme", "themeActive");
   }
 });
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  breakpoints: {
+    700: {
+      slidesPerView: 2
+    },
+    1200: {
+      slidesPerView: 3
+    }
+  }
+});
