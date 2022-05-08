8fabe4aa2599291a6594c15d3add104c43a639cc
ItC <ivan120536@gmail.com>
2022-05-09 02:12:33 +0400

    imdone

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 from main import app, mail, db_session
 
 if __name__ == "__main__":
-    db_session.global_init("db/mainDB.sqlite")
+    db_session.global_init("mainDB.sqlite")
     mail.startMailServer()
     app.run(host='0.0.0.0', port=80)
     mail.stopMailServer()
